c8f980b12c3623c4e719fadebbe0e17484cba0f9
frutos46 <jantonio.frutos92@gmail.com>
2022-03-28 15:35:37 +0200

    Add files via upload

---
+++ Backup Zona DNS.ps1
@@ -0,0 +1,38 @@
+﻿####################################################################################
+# Autor: Jose Antonio Frutos
+# Version: v1
+# Creado: 28/03/2022
+####################################################################################
+
+$DNSSERVER=gc env:computername
+
+$BkfFolder=”c:\windows\system32\dns\backup”
+
+if ( -not(test-path $BkfFolder)) {
+new-item $BkfFolder -Type Directory | Out-Null
+} else {
+
+Remove-Item $BkfFolder”\*” -recurse
+}
+
+
+$StrFile=$Bkffolder+”\input.csv”
+Add-content $StrFile “Zone,ZoneType,Update,Secondary,DsIntegrated”
+
+
+
+$List = gwmi -ComputerName $DNSSERVER -Namespace root\MicrosoftDNS -Class MicrosoftDNS_Zone
+
+$List | foreach {
+
+$line=$_.name+”,”+$_.ZoneType+”,”+$_.AllowUpdate+”,”+$_.MasterServers+”,”+$_.DsIntegrated
+Add-content $Strfile $Line
+
+$ZoneName=$_.name
+$StrPath=”\backup\”+$_.name
+
+
+
+dnscmd $DNSSERVER “/ZoneExport” $ZoneName $StrPath
+
+}
