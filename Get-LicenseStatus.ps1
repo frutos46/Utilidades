ff80eb0abea7ba98903c6ce0f94cab75cfcbecd7
frutos46 <jantonio.frutos92@gmail.com>
2022-04-05 10:05:03 +0200

    v1

---
+++ Get-LicenseStatus.ps1
@@ -0,0 +1,11 @@
+﻿    ###################################################################################
+    # Autor: Jose Antonio Frutos
+    # Date: 05/04/2022
+    # Version v1
+    ###################################################################################
+    
+    Get-CimInstance -ClassName SoftwareLicensingProduct -ComputerName "HPS" |` 
+    Where{$_.PartialProductKey -and $_.Name -like "*Windows*"} | Select ` 
+    @{Expression={$_.PSComputerName};Name="ComputerName"},` 
+    @{Expression={$_.Name};Name="WindowsName"} ,"ApplicationID",` 
+    @{Expression={$LicenseStatus[$($_.LicenseStatus)]};Name="LicenseStatus"} 
